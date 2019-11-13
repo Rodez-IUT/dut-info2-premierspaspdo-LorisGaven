1e29c31550e213444c2e6570255fe315d166ff74
LorisGaven <loris.gaven@iut-rodez.fr>
2019-11-13 10:19:15 +0100

    fix #3.2 Moteur de recherche sur status et username

--- all_users.php
+++ all_users.php
@@ -3,6 +3,17 @@
 	<head>
 		<meta charset="utf-8">
 		<title>All users</title>
+		<style>
+			table {
+				border-collapse: collapse;
+				width: 100%;
+			}
+			th, td {
+				padding: 8px;
+				text-align: left;
+				border-bottom: 1px solid #ddd;
+			}
+		</style>
 	</head>
 	<body>
 		<?php
@@ -24,6 +35,16 @@
 			}
 		?>
 		<h1>All users</h1>
+		<form action="all_users.php" method="post">
+			Start with letter : <input type="text" name="lettre">
+			and status is : <select name="status">
+								<option value="all">All</option>
+								<option value="Active account">Active account</option>
+								<option value="Waiting for account validation">Waiting for account validation</option>
+								<option value="Waiting for account deletion">Waiting for account deletion</option>
+							</select>
+			<button type="submit">OK</button>
+		</form>
 		<table>
 			<tr>
 				<th>Id</th>
@@ -32,12 +53,19 @@
 				<th>Status</th>
 			</tr>
 			<?php
-				$stmt = $pdo->query('SELECT * FROM users JOIN status ON status_id = status.id ORDER BY username');
+				if (isset($_POST['lettre'])) {
+					$first_letter = $_POST['lettre'];
+				}
+				if (isset($_POST['status'])) {
+					$status = $_POST['status'];
+				}
+				$stmt = $pdo->query('SELECT users.id as user_id, username, email, status.name FROM users JOIN status ON status_id = status.id ORDER BY username');
 				while ($row = $stmt->fetch())
 				{
-					if ($row['name'] == "Active account" && substr($row['username'], 0, 1) == 'e') {
+					if (isset($_POST['lettre']) && isset($_POST['status']) && ($row['name'] == $status || $status == "all")
+					&& (substr($row['username'], 0, 1) == $first_letter || $first_letter == "")) {
 						echo "<tr>";
-						echo "<td>".$row['id']."</td>";
+						echo "<td>".$row['user_id']."</td>";
 						echo "<td>".$row['username']."</td>";
 						echo "<td>".$row['email']."</td>";
 						echo "<td>".$row['name']."</td>";
