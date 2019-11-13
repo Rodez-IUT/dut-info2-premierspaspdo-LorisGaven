f24d0b3b6df831264a046f2f33d81f01a10dcee7
LorisGaven <loris.gaven@iut-rodez.fr>
2019-11-13 09:34:14 +0100

    fix #2 Récupération et affichage des utilisateurs avec énoncé non préparé

--- all_users.php
+++ all_users.php
@@ -32,14 +32,14 @@
 				<th>Status</th>
 			</tr>
 			<?php
-				$stmt = $pdo->query('SELECT * FROM users ORDER BY username');
+				$stmt = $pdo->query('SELECT * FROM users JOIN status ON status_id = status.id ORDER BY username');
 				while ($row = $stmt->fetch())
 				{
 					echo "<tr>";
 					echo "<td>".$row['id']."</td>";
 					echo "<td>".$row['username']."</td>";
 					echo "<td>".$row['email']."</td>";
-					echo "<td>".$row['status_id']."</td>";
+					echo "<td>".$row['name']."</td>";
 					echo "</tr>";
 				}
 			?>
